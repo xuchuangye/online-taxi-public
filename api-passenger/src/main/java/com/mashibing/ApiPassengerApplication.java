f07268461bdea80b56ed4303be021061b89f8541
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2023-02-06 09:19:24 +0800

    api-passenger项目的骨架搭建

---
+++ api-passenger/src/main/java/com/mashibing/ApiPassengerApplication.java
@@ -0,0 +1,16 @@
+package com.mashibing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xcy
+ * @date 2023/2/6 - 9:13
+ */
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPassengerApplication.class, args);
+	}
+}
---
+++ api-passenger/src/main/java/com/mashibing/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.mashibing.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xcy
+ * @date 2023/2/6 - 9:15
+ */
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "test api passenger";
+	}
+}
