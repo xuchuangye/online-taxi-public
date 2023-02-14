37161dd8ed042d13831702b59a1262d4402c2a6e
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2023-02-14 18:22:22 +0800

    service-price微服务远程调用service-map微服务

---
+++ service-price/src/main/java/com/mashibing/serviceprice/remote/ServiceMapClient.java
@@ -0,0 +1,19 @@
+package com.mashibing.serviceprice.remote;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.ForecastPriceDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author xcy
+ * @date 2023/2/14 - 17:30
+ */
+@FeignClient("service-map")
+public interface ServiceMapClient {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/direction/driving")
+	public ResponseResult directionDriving(@RequestBody ForecastPriceDTO forecastPriceDTO);
+}
--- service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/ForecastPriceService.java
@@ -3,6 +3,7 @@ package com.mashibing.serviceprice.service;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import com.mashibing.internalcommon.request.ForecastPriceDTO;
 import com.mashibing.internalcommon.response.ForecastPriceResponse;
+import com.mashibing.serviceprice.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,29 +16,26 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class ForecastPriceService {
 
-	public ResponseResult forecastPrice(ForecastPriceDTO forecastPriceDTO) {
+	@Autowired
+	private ServiceMapClient serviceMapClient;
 
-		/*
-		String depLongitude = forecastPriceDTO.getDepLongitude();
-		String depLatitude = forecastPriceDTO.getDepLatitude();
-		String destLongitude = forecastPriceDTO.getDestLongitude();
-		String destLatitude = forecastPriceDTO.getDestLatitude();
+	public ResponseResult forecastPrice(String depLongitude, String depLatitude, String destLongitude, String destLatitude) {
+
 		log.info("出发地经度: " + depLongitude);
 		log.info("出发低纬度: " + depLatitude);
 		log.info("目的地经度: " + destLongitude);
-		log.info("目的地纬度: " + destLatitude);*/
-
-		//调用第三方接口API
-
-
+		log.info("目的地纬度: " + destLatitude);
 
 		log.info("调用地图服务，查询距离和时长");
+		ForecastPriceDTO forecastPriceDTO = new ForecastPriceDTO();
+		forecastPriceDTO.setDepLongitude(depLongitude);
+		forecastPriceDTO.setDepLatitude(depLatitude);
+		forecastPriceDTO.setDestLongitude(destLongitude);
+		forecastPriceDTO.setDestLatitude(destLatitude);
+		//调用第三方接口API
 
 		log.info("读取计价规则");
 		log.info("根据距离、时长、计价规则，计算价格");
-
-		ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
-		forecastPriceResponse.setPrice(12.36);
-		return ResponseResult.success(forecastPriceResponse);
+		return serviceMapClient.directionDriving(forecastPriceDTO);
 	}
 }
