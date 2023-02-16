003bf514c372e5fd8c5040425008062484aa57b4
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2023-02-16 18:01:12 +0800

    api-driver微服务的创建以及注册到Nacos注册中心

--- api-driver/src/main/java/com/mashibing/apidriver/ApiDriverApplication.java
+++ api-driver/src/main/java/com/mashibing/apidriver/ApiDriverApplication.java
@@ -1,5 +1,18 @@
-package com.mashibing.apidriver;/**
-@author xcy    
-@date 2023/2/16 - 17:50     
-*/    public class ApiDriverApplication {
+package com.mashibing.apidriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xcy
+ * @date 2023/2/16 - 17:50
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDriverApplication.class, args);
+	}
 }
--- api-driver/src/main/java/com/mashibing/apidriver/controller/DriverUserController.java
+++ api-driver/src/main/java/com/mashibing/apidriver/controller/DriverUserController.java
@@ -1,5 +1,18 @@
-package com.mashibing.apidriver.controller;/**
-@author xcy    
-@date 2023/2/16 - 17:53     
-*/    public class DriverUserController {
+package com.mashibing.apidriver.controller;
+
+import com.mashibing.internalcommon.dto.ResponseResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xcy
+ * @date 2023/2/16 - 17:53
+ */
+@RestController
+public class DriverUserController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "api driver";
+	}
 }
