7be9b13f113226cb2e9bd2b9b685e675f2775d54
xuchuangye <59134197+xuchuangye@users.noreply.github.com>
2023-02-16 17:59:42 +0800

    api-driver微服务的创建以及注册到Nacos注册中心

---
+++ api-driver/src/main/java/com/mashibing/apidriver/ApiDriverApplication.java
@@ -0,0 +1,5 @@
+package com.mashibing.apidriver;/**
+@author xcy    
+@date 2023/2/16 - 17:50     
+*/    public class ApiDriverApplication {
+}
---
+++ api-driver/src/main/java/com/mashibing/apidriver/controller/DriverUserController.java
@@ -0,0 +1,5 @@
+package com.mashibing.apidriver.controller;/**
+@author xcy    
+@date 2023/2/16 - 17:53     
+*/    public class DriverUserController {
+}
